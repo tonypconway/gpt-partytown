da390a0cf8ec5fc893de412c2f25dfb147ee6680
Harry Theodoulou <harrytheo@google.com>
2022-08-05 14:39:08 +0100

    Added partytown

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+app.use(express.static('node_modules/@builder.io/'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
